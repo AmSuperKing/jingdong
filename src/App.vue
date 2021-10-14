8ae42fcef36aefe012ad304653ecee9ac857ebc9
wangjian <wangjian>
2021-10-14 17:08:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe6fa;</span>
+      广东省佛山市三水区万达广场2栋B座
+      <span class="iconfont position__notice">&#xe8fc;</span>
+    </div>
+  </div>
+  <div class="docker">
+    <!-- BEM(Block__Element--Modifier) 命名规则，双下划线双横线 -->
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6bb;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe645;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe60c;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe6b3;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import './styles/variables.scss';
+@import './styles/mixins.scss';
+
+.wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: .5rem;
+  padding: 0 .18rem;
+}
+.position {
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  @include ellipsis;
+  color: $content-fontcolor;
+  .position__icon {
+    position: relative;
+    top: .02rem;
+    font-size: .2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+}
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: .01rem solid #f1f1f1;
+  color: $content-fontcolor;
+  // & 符号表示外层的修饰符，用于引用父元素
+  // & 表示 .docker, &__item 等于 .docker__title
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+  &__title {
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
+</style>
